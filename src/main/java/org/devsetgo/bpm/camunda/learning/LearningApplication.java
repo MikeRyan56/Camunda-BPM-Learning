11fafead1abc9821ff75e43fd3e50685efb5cadc
Stu Ryan <mikeryan56@gmail.com>
2018-06-16 14:30:02 -0400

    Adding embedded forms.

--- src/main/java/org/devsetgo/bpm/camunda/learning/LearningApplication.java
+++ src/main/java/org/devsetgo/bpm/camunda/learning/LearningApplication.java
@@ -2,9 +2,17 @@ package org.devsetgo.bpm.camunda.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
 
 @SpringBootApplication
 public class LearningApplication {
+
+    @Autowired
+    private RuntimeService runtimeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
